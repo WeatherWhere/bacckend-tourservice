69d7e6e3a00cd8794999674147bf5523902d394b
j2woo <kjk04021@naver.com>
2023-04-07 02:45:20 +0900

    (feat: Exception) 예외처리

--- src/main/java/com/example/backendtourservice/service/weatherair/WeatherAirDataServiceImpl.java
+++ src/main/java/com/example/backendtourservice/service/weatherair/WeatherAirDataServiceImpl.java
@@ -9,8 +9,11 @@ import java.util.List;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.example.backendtourservice.dto.ResultDTO;
 import com.example.backendtourservice.dto.weatherair.AirForecastDTO;
 import com.example.backendtourservice.dto.weatherair.WeatherAirDataDTO;
 import com.example.backendtourservice.dto.weatherair.WeatherMidDTO;
@@ -23,7 +26,7 @@ import lombok.extern.log4j.Log4j2;
 public class WeatherAirDataServiceImpl implements WeatherAirDataSerivce {
 
     // 해당 지역, 예보 날짜 Air api 대기 주간예보 url
-    private String makeAirUrl(LocalDate date,String city) throws URISyntaxException {
+    private String makeAirUrl(LocalDate date,String city) {
         String baseUrl="http://localhost:8090/air/forecast/tour/data?";
         String baseDate="baseDate="+date;
         String region="&city="+city;
@@ -32,7 +35,7 @@ public class WeatherAirDataServiceImpl implements WeatherAirDataSerivce {
     }
 
     // 해당 지역, 예보 날짜 Air api 대기 주간예보 url
-    private String makeWeatherUrl(String regionCode, LocalDate date) throws URISyntaxException {
+    private String makeWeatherUrl(String regionCode, LocalDate date) {
         // weather db에 BaseDate 형식:"20231011"이므로 LocalDate -> String으로 바꿔주는 과정 필요
         String sDate=date+"";
         sDate=sDate.replaceAll("-","");
@@ -93,7 +96,7 @@ public class WeatherAirDataServiceImpl implements WeatherAirDataSerivce {
         return  dto;
     }
 
-    private WeatherMidDTO getWeatherMid(String regionCode, LocalDate date) throws URISyntaxException, ParseException {
+    private WeatherMidDTO getWeatherMid(String regionCode, LocalDate date) throws ParseException {
         RestTemplate restTemplate= new RestTemplate();
         // RestTemplate으로 날씨 중기예보 data 받아오기
         String result=restTemplate.getForObject(makeWeatherUrl(regionCode,date),String.class);
@@ -103,7 +106,7 @@ public class WeatherAirDataServiceImpl implements WeatherAirDataSerivce {
         return weatherMidDTO;
     }
 
-    private AirForecastDTO getAirForecast(String city, LocalDate date) throws ParseException, URISyntaxException {
+    private AirForecastDTO getAirForecast(String city, LocalDate date) throws ParseException{
         RestTemplate restTemplate= new RestTemplate();
         // RestTemplate으로 대기 주간예보 data 받아오기
         String result=  restTemplate.getForObject(makeAirUrl(date,city),String.class);
@@ -118,32 +121,44 @@ public class WeatherAirDataServiceImpl implements WeatherAirDataSerivce {
 
     // 대기, 날씨 합친 데이터
     @Override
-    public List<WeatherAirDataDTO> updateWeatherAirData(String regionCode, LocalDate date) throws
-        ParseException,
-        URISyntaxException {
-        List<WeatherAirDataDTO> list=new ArrayList<>();
-        for(int i=0; i<6; i++){
-            LocalDate searchDate=date.plusDays(i);// 호출한 LocalDate 객체에 일(day)이 더해진 LocalDate 객체를 반환합니다.
-            WeatherMidDTO weatherMidDTO=getWeatherMid(regionCode,searchDate);
-            // 대기 받아오기
-            AirForecastDTO airForecastDTO=getAirForecast(weatherMidDTO.getCity(), searchDate);
-            WeatherAirDataDTO dto=WeatherAirDataDTO.builder()
-                .regionCode(weatherMidDTO.getRegionCode())
-                .baseTime(airForecastDTO.getBaseDate())
-                .regionName(weatherMidDTO.getRegionName())
-                .city(weatherMidDTO.getCity())
-                .tmn(weatherMidDTO.getTmn())
-                .tmx(weatherMidDTO.getTmx())
-                .rAm(weatherMidDTO.getRAm())
-                .rPm(weatherMidDTO.getRPm())
-                .wAm(weatherMidDTO.getWAm())
-                .wPm(weatherMidDTO.getWPm())
-                .reliability(airForecastDTO.getReliability())
-                .forecast(airForecastDTO.getForecast())
-                .build();
-            log.info("날씨, 대기 데이터: {}",dto);
-            list.add(dto);
+    public List<WeatherAirDataDTO> updateWeatherAirData(String regionCode, LocalDate date) {
+        try{
+            List<WeatherAirDataDTO> list=new ArrayList<>();
+            for(int i=0; i<6; i++){
+                LocalDate searchDate=date.plusDays(i);// 호출한 LocalDate 객체에 일(day)이 더해진 LocalDate 객체를 반환합니다.
+                WeatherMidDTO weatherMidDTO=getWeatherMid(regionCode,searchDate);
+                // 대기 받아오기
+                AirForecastDTO airForecastDTO=getAirForecast(weatherMidDTO.getCity(), searchDate);
+                WeatherAirDataDTO dto=WeatherAirDataDTO.builder()
+                    .regionCode(weatherMidDTO.getRegionCode())
+                    .baseTime(airForecastDTO.getBaseDate())
+                    .regionName(weatherMidDTO.getRegionName())
+                    .city(weatherMidDTO.getCity())
+                    .tmn(weatherMidDTO.getTmn())
+                    .tmx(weatherMidDTO.getTmx())
+                    .rAm(weatherMidDTO.getRAm())
+                    .rPm(weatherMidDTO.getRPm())
+                    .wAm(weatherMidDTO.getWAm())
+                    .wPm(weatherMidDTO.getWPm())
+                    .reliability(airForecastDTO.getReliability())
+                    .forecast(airForecastDTO.getForecast())
+                    .build();
+                log.info("날씨, 대기 데이터: {}",dto);
+                list.add(dto);
+            }
+            return list;
+        }catch (ParseException e){
+            // json 데이터 파싱할 때 error
+            e.printStackTrace();
+            log.error("ParseException이 발생");
+            //return ResultDTO.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "ParseException이 발생했습니다.", null);
+            // 수정해야함
+            return null;
+        }catch (Exception e) {
+            e.printStackTrace();
+            log.error("예기치 못한 에러가 발생");
+           // return ResultDto.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "예기치 못한 에러가 발생했습니다.", null);
+            return null;
         }
-        return list;
     }
 }

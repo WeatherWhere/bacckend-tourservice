8fa321da06e307f168a1195d47ff8a244ff1d888
hwang-jubin <117631618+hwang-jubin@users.noreply.github.com>
2023-04-12 15:47:33 +0900

    (fix: test code 주석 제거)

--- src/test/java/com/example/backendtourservice/WeatherAirDataTests.java
+++ src/test/java/com/example/backendtourservice/WeatherAirDataTests.java
@@ -1,12 +1,12 @@
-//package com.example.backendtourservice;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import com.example.backendtourservice.service.weatherair.WeatherAirDataSerivce;
-//
-//@SpringBootTest
-//public class WeatherAirDataTests {
-//
-//
-//}
+package com.example.backendtourservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.backendtourservice.service.weatherair.WeatherAirDataSerivce;
+
+@SpringBootTest
+public class WeatherAirDataTests {
+
+
+}

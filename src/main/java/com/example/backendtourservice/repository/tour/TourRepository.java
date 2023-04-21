bdaf62fc66653ccea5d797dbcb2650a19307619c
itpark1018 <itpark1018@gmail.com>
2023-04-21 16:50:43 +0900

    (fix:searchservice) 검색조건에 contentTypeId 추가

--- src/main/java/com/example/backendtourservice/repository/tour/TourRepository.java
+++ src/main/java/com/example/backendtourservice/repository/tour/TourRepository.java
@@ -3,7 +3,6 @@ package com.example.backendtourservice.repository.tour;
 import com.example.backendtourservice.domain.tour.TourEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,10 +12,10 @@ public interface TourRepository extends JpaRepository<TourEntity, Long> {
     List<TourEntity> findBySigunguCodeAndAreaCode(Integer sigunguCode, Integer areaCode);
 
 
-    @Query(value = "SELECT * FROM tour.tour_spot WHERE ST_DWithin("
+    @Query(value = "SELECT * FROM tour.tour_spot WHERE content_type_id = ? AND ST_DWithin("
             + "(ST_MakePoint(CAST(location_x AS double precision), CAST(location_y AS double precision))), "
             + "ST_SetSRID(ST_MakePoint(?, ?), 4326), "
-            + "20000, true)", nativeQuery = true)
-    List<TourEntity> findBySql(double x, double y);
+            + "10000, true)", nativeQuery = true)
+    List<TourEntity> findBySql(int contentTypeId, double x, double y);
 
 }
